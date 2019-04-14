f7228b493edd6e8375236f3aa05ac41676a7b2a2
taketakekaho <kahori1107.com@gmail.com>
2019-04-15 02:47:16 +0900

    詳細ページビューのview,pyを設定

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from .models import Blog
+from django.views.generic import DetailView
 
 # リストビュー　https://docs.djangoproject.com/ja/2.1/ref/class-based-views/generic-display/#listview
 class BlogListView(ListView):
+    model = Blog
+
+# ディテールビュー　https://docs.djangoproject.com/ja/2.1/ref/class-based-views/generic-display/#detailview
+class BlogDetailView(DetailView):
     model = Blog
