0728ef5d822a93da41ad79ece558386c6cf98b70
taketakekaho <kahori1107.com@gmail.com>
2019-04-15 01:17:01 +0900

    databaseのマイグレーションを実施

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Blog(models.Model):
     #投稿
-    content = models.CharFierld(max_length=140)
+    content = models.CharField(max_length=140)
     #投稿日時:自動的に作成時点の日時を保存
-    posted_date = models.DateTimeField(auto_now_add=true)
+    posted_date = models.DateTimeField(auto_now_add=True)
