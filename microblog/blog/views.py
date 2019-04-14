9b41393fd78f7b04edce16f14218fea2886d3afa
taketakekaho <kahori1107.com@gmail.com>
2019-04-15 02:05:31 +0900

    ドキュメントURLコメント追記

--- microblog/blog/views.py
+++ microblog/blog/views.py
@@ -3,5 +3,6 @@ from django.views.generic import ListView
 
 from .models import Blog
 
+# リストビュー　https://docs.djangoproject.com/ja/2.1/ref/class-based-views/generic-display/#listview
 class BlogListView(ListView):
     model = Blog
